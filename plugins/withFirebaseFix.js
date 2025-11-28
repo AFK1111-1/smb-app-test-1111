e63a926836beb4451e3caeb40b710f7360f0ba37
afkarfase <afkar.f@insighture.com>
2025-11-28 20:40:51 +0530

    fix: tryout ii from winsurf

--- plugins/withFirebaseFix.js
+++ plugins/withFirebaseFix.js
@@ -28,17 +28,32 @@ const withFirebaseFix = (config) => {
   post_install do |installer|
     installer.pods_project.targets.each do |target|
       target.build_configurations.each do |config|
+        # Common build settings
         config.build_settings['CLANG_WARN_NON_MODULAR_INCLUDE_IN_FRAMEWORK_MODULES'] = 'NO'
         config.build_settings['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'NO'
+        
+        # Swift settings
+        config.build_settings['SWIFT_VERSION'] = '5.0'
+        config.build_settings['SWIFT_OPTIMIZATION_LEVEL'] = '-Onone'
+        config.build_settings['SWIFT_COMPILATION_MODE'] = 'wholemodule'
         
         # Fix for Xcode 16.1
         config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
         config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
         
         # Enable modular headers for Firebase
-        if target.name == 'RNFBApp' || target.name == 'RNFBMessaging'
+        if (target.name.include?('RNFB') || 
+            target.name.include?('Firebase') ||
+            target.name.include?('Google') ||
+            target.name.include?('GTM') ||
+            target.name.include?('Promises') ||
+            target.name.include?('GoogleDataTransport') ||
+            target.name.include?('GoogleUtilities') ||
+            target.name.include?('nanopb') ||
+            target.name.include?('leveldb')) {
           config.build_settings['DEFINES_MODULE'] = 'YES'
-        end
+          config.build_settings['ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES'] = 'YES'
+        }
       end
     end
   end
