697f9f1146ccfc83ea52d746cbb262a4eba7afaf
afkarfase <afkar.f@insighture.com>
2025-11-28 15:39:58 +0530

    Remove unnecessary extraPodspecProperties for iOS configuration in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -62,10 +62,6 @@ export default {
           ios: {
             useFrameworks: 'static',
             enableModulePrecompilation: false,
-            extraPodspecProperties: {
-              'CLANG_WARN_NON_MODULAR_INCLUDE_IN_FRAMEWORK_MODULES': 'NO',
-              'GCC_TREAT_WARNINGS_AS_ERRORS': 'NO',
-            },
           },
           android: {
             enableProguardInReleaseBuilds: true,
